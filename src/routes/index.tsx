3f58a80a4ecd5c0bfc55340ecbbb084b96e5d38d
eduardo <duds.deve@gmail.com>
2025-09-29 14:10:17 -0300

    fix new

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,7 +10,8 @@ import Teste from '../pages/index'
 export function Router() {
     return (
         <Routes>
-    
+          <Route path="/" element={<div>Acesso negado - URL inválida</div>} />
+
         <Route path="/autenticacao/:slug" element={<Teste/>} />
 
       
