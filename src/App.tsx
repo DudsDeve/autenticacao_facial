10a338c4d43f369d6a653c629d93cf2b06fe9988
eduardo <duds.deve@gmail.com>
2025-09-29 14:47:45 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,16 @@ import { theme } from "@styles/theme";
 import { ThemeProvider } from "@emotion/react";
 import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
 import { muiTheme } from '@styles/theme/mui';
-import { AppProviders } from "./AppProviders";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <MuiThemeProvider theme={muiTheme}>
-        <AppProviders>
+       
           <BrowserRouter>
             <Router />
           </BrowserRouter>
           <GlobalStyle />
-        </AppProviders>
       </MuiThemeProvider>
     </ThemeProvider>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,40 +3,17 @@
 import { Route, Routes } from 'react-router-dom'
 
 
-import { DefaultLayout } from '../template/DefaultLayout'
-import { Dashboard } from '@pages/Dashboard'
-import { Leads } from '@pages/Leads'
-import {  Resumes } from '@pages/HR/Resumes'
+
 import Teste from '@pages/Teste/index'
-import { SalesDashboard } from '@pages/SalesDashboard'
-import { OperationGroup } from '@pages/OperationGroup'
-import { Kanban } from '@pages/HR/Kanban'
-import { Jobs } from '@pages/HR/Jobs'
-import { HrMessagesPage } from '@pages/HR/HrMessages'
-import Whatsapp from '@pages/Whatsapp'
-import { HrDashboard } from '@pages/HR/Dashboard'
 
 
 
 export function Router() {
     return (
         <Routes>
-        <Route path = "/" element = {< DefaultLayout />}>
-            <Route index  path = "/dashboard"element = {< Dashboard />} />
-            <Route path = "/whatsapp"element = {< Whatsapp />} />
-            <Route path = "/leads"element = {< Leads />} />
-            <Route path = "/resumes"element = {< Resumes/>} />
-            <Route path = "/teste"element = {< Teste/>} />
-            <Route path = "/sales-dashboard"element = {< SalesDashboard/>} />
-            <Route path = "/operation-group"element = {< OperationGroup/>} />
-            <Route path = "/kanban-rh"element = {< Kanban/>} />
-            <Route path = "/jobs"element = {< Jobs/>} />
-            <Route path = "/hr-messages"element = {< HrMessagesPage/>} />
-            <Route index  path = "/dashboard-hr"element = {< HrDashboard />} />
-            <Route path="/teste/:slug" element={<Teste />} />
+                    <Route path="/autenticacao/:slug" element={<Teste />} />
 
 
-        </Route>
 
 </Routes>
   )
